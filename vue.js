0b2283f6e840e1975ca50b933b280f7113f115d2
Carlos Cordova <deobertinho@gmail.com>
2020-07-31 17:19:52 -0500

    Iniciando con Vue.js

---
+++ vue.js
@@ -0,0 +1,9 @@
+var app  = new Vue({
+    el: '#app',
+    data:{
+        product: 'Socks',
+        description: 'Recibiendo productos',
+        image: 'vmSocks-green.jpg',
+        inventario: 0
+    }
+})
