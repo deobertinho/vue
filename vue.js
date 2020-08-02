952dd44cc1ca21aea59219408056ff6350232527
Carlos Cordova <deobertinho@gmail.com>
2020-08-01 22:16:07 -0500

    cambio de imagen y botones en vue

--- vue.js
+++ vue.js
@@ -4,6 +4,35 @@ var app  = new Vue({
         product: 'Socks',
         description: 'Recibiendo productos',
         image: 'vmSocks-green.jpg',
-        inventario: 0
+        stock: 11,
+        onSale: true,
+        details: ["80% cotton", "20% polyster", "Gender-neutral"],
+        features: ["Sin Costura", "Compresión", "Altura", "Material", "Puntera"],
+        variants:[
+            {
+                variantID:2234,
+                variantColor: "Green",
+                variantImage: 'vmSocks-green.jpg'
+            },
+            {
+                variantId:2235,
+                variantColor:"Blue",
+                variantImage: 'vmSocks-blue.jpg'
+            }
+        ],
+        cart: 0
+    },
+    methods:{
+        addToCart(){
+            this.cart += 1
+            // this.stock -=1
+        },
+        deltoCart(){
+            this.cart -=1
+            // this.stock +=1
+        },
+        updateProduct(variantImage){
+            this.image = variantImage
+        }
     }
 })
